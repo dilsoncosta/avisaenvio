e81f4836f711f5d80d1e23549920115d344b2800
“Saf+” <system@safmaishmg.com.br>
2024-03-13 08:56:56 -0300

    Ajuste favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Avisa Envio</title>
-		<link rel="icon" type="image/png" href="<%= BASE_URL %>images/favicon.png">
+		<link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 		@vite(['resources/js/App.js', 'resources/css/App.css' ]) 
