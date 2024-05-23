b43da8ea231bfa8eb0daff1036f64a957523cd2c
“Saf+” <system@safmaishmg.com.br>
2024-05-23 18:35:21 -0300

    Testando deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Avisa Envio</title>
+		<title>Avisa Envio - Teste</title>
 		<link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
