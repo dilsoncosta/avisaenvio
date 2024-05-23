efc20564a48ec5a894a3656504513bd9a2fce5e5
“Saf+” <system@safmaishmg.com.br>
2024-05-23 17:24:28 -0300

    ajuste README

--- server_deploy_prd.sh
+++ server_deploy_prd.sh
@@ -4,47 +4,13 @@ set -e
 
 echo "Deploying application..."
 
-# Update codebase outside the container
-git stash
-git pull origin main
+# Enter maintenance mode
+(docker exec avisapp_app_1 php artisan down) || true
 
-# Executar comandos dentro do contêiner Docker
-docker exec  avisapp_app_1 bash -c '
-  # Enter maintenance mode with a custom message 
-  php artisan down --render="deploy"
+#Update codebase
+git fetch origin main
+git reset --hard origin/main
 
-  # Install dependencies based on lock file
-  composer install --no-interaction --prefer-dist --optimize-autoloader
+# Install dependencies based on lock file
+docker exec avisapp_app_1 composer install
 
-  # Migrate database
-  php artisan migrate --force
-
-  # Note: If you're using queue workers, this is the place to restart them.
-  php artisan queue:restart
-
-  # Register Permissions
-  php artisan db:seed --class=PermissionsTableSeeder
-
-  # Clear cache config
-  php artisan config:clear
-  php artisan cache:clear
-  php artisan route:clear
-  php artisan view:clear
-
-  php artisan config:cache
-  php artisan route:cache
-  php artisan view:cache
-
-  # Clear cache
-  php artisan optimize:clear
-
-  # Build frontend
-  npm install
-  npm run build
-
-  # Exit maintenance mode 
-  php artisan up
-'
-
-
-echo "Application deployed!"
