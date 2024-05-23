bb1095478d0446202fdcb2eb07d76e781dc13018
“Saf+” <system@safmaishmg.com.br>
2024-05-23 16:47:21 -0300

    ajuste no arquivo ssh de deploy

--- server_deploy_prd.sh
+++ server_deploy_prd.sh
@@ -4,46 +4,46 @@ set -e
 
 echo "Deploying application..."
 
-# Enter maintenance mode with a custom message 
-php artisan down --render="deploy"
-
-# Update codebase 
+# Update codebase outside the container
 git stash
 git pull origin main
 
+# Executar comandos dentro do contêiner Docker
+docker exec -it avisapp_app_1 bash -c '
+  # Enter maintenance mode with a custom message 
+  php artisan down --render="deploy"
 
-# Install dependencies based on lock file
-composer install --no-interaction --prefer-dist --optimize-autoloader
+  # Install dependencies based on lock file
+  composer install --no-interaction --prefer-dist --optimize-autoloader
 
-# Migrate database
-php artisan migrate --force
+  # Migrate database
+  php artisan migrate --force
 
-# Note: If you're using queue workers, this is the place to restart them.
-php artisan queue:restart
+  # Note: If you're using queue workers, this is the place to restart them.
+  php artisan queue:restart
 
+  # Register Permissions
+  php artisan db:seed --class=PermissionsTableSeeder
 
-# Register Permissions
-php artisan db:seed --class=PermissionsTableSeeder
+  # Clear cache config
+  php artisan config:clear
+  php artisan cache:clear
+  php artisan route:clear
+  php artisan view:clear
 
-# Clear cache config
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
+  php artisan config:cache
+  php artisan route:cache
+  php artisan view:cache
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+  # Clear cache
+  php artisan optimize:clear
 
-# Clear cache
-php artisan optimize:clear
+  # Build frontend
+  npm install
+  npm run build
 
-# Build frontend
-npm install
-
-npm run build
-
-# Exit maintenance mode 
-php artisan up
+  # Exit maintenance mode 
+  php artisan up
+'
 
 echo "Application deployed!"
