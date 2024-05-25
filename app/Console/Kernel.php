e5ceb99a1483b28dfdd17e938850865da39a3918
“Saf+” <system@safmaishmg.com.br>
2024-05-25 08:56:34 -0300

    Ajuste de bug no horario de disparo dos agendamentos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,9 +9,9 @@ class Kernel extends ConsoleKernel
 {
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('send:download_order_best_shipping')->everyThirtySeconds()->withoutOverlapping();
-		$schedule->command('send:download_order_nuvem_shop')->everyThirtySeconds()->withoutOverlapping();
-		$schedule->command('send:download_events_order')->everyFifteenSeconds()->withoutOverlapping();
+		$schedule->command('send:download_order_best_shipping')->everyThirtyMinutes()->withoutOverlapping();
+		$schedule->command('send:download_order_nuvem_shop')->everyThirtyMinutes()->withoutOverlapping();
+		$schedule->command('send:download_events_order')->everyFifteenMinutes()->withoutOverlapping();
 		$schedule->command('send:send_msg_one_guest')->hourly()->between('08:00', '12:00')->withoutOverlapping();
 		$schedule->command('send:send_msg_two_guest')->hourly()->between('08:00', '12:00')->withoutOverlapping();
 		$schedule->command('send:send_msg_three_guest')->hourly()->between('08:00', '12:00')->withoutOverlapping();
