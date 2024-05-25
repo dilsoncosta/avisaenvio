c8c608f238a75a78f1b318224c10f0c8b511d5e6
“Saf+” <system@safmaishmg.com.br>
2024-05-25 12:09:52 -0300

    Troca de servidor

--- server_deploy_hmg.sh
+++ server_deploy_hmg.sh
@@ -2,49 +2,44 @@
 
 set -e
 
-APP_NAME='avisapp_app_1'
-
 echo "Deploying application..."
 
+# Enter maintenance mode with a custom message 
+php artisan down --render="deploy"
 
-# Enter maintenance mode
-(docker exec ${APP_NAME} php artisan down --render="deploy") || true
+# Update codebase 
+git stash
+git pull origin hmg
 
-#Update codebase
-git fetch origin hmg
-git reset --hard origin/hmg
 
 # Install dependencies based on lock file
-docker exec ${APP_NAME} composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
 # Migrate database
-docker exec ${APP_NAME} php artisan migrate --force
+php artisan migrate --force
 
 # Note: If you're using queue workers, this is the place to restart them.
-docker exec ${APP_NAME} php artisan queue:restart
+php artisan queue:restart
+
 
 # Register Permissions
-docker exec ${APP_NAME} php artisan db:seed --class=PermissionsTableSeeder
+php artisan db:seed --class=PermissionsTableSeeder
 
 # Clear cache config
-docker exec ${APP_NAME} php artisan config:clear
-docker exec ${APP_NAME} php artisan cache:clear
-docker exec ${APP_NAME} php artisan route:clear
-docker exec ${APP_NAME} php artisan view:clear
-
-docker exec ${APP_NAME} php artisan config:cache
-docker exec ${APP_NAME} php artisan route:cache
-docker exec ${APP_NAME} php artisan view:cache
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
 
 # Clear cache
-docker exec ${APP_NAME} php artisan optimize:clear
+php artisan optimize:clear
 
 # Build frontend
-docker exec ${APP_NAME} npm install
+npm install
 
-docker exec ${APP_NAME} npm run build
+npm run build
 
 # Exit maintenance mode
-docker exec ${APP_NAME} php artisan up
+php artisan up
 
 echo "Application deployed!"
